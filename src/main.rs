72279bfd45bfd4681ef2dc7927d93d7e09ad5fd7
Johnny Kao <kaojohnny@gmail.com>
2022-02-26 10:59:52 +0800

    Send pinboard popular link to telegram

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,40 @@ async fn fetch_pins() -> Result<Vec<Pin>, Box<dyn std::error::Error>> {
     Ok(pins)
 }
 
+async fn post_to_tg_channel(pin: Pin) -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    let path = format!(
+        "https://api.telegram.org/bot{}/sendMessage",
+        dotenv!("TG_BOT_TOKEN")
+    );
+    let text = format!("{}\n{}\n{}", pin.d, pin.u, pin.t.join(", "));
+    client
+        .get(path)
+        .query(&[("chat_id", dotenv!("TG_CHAT_ID")), ("text", &text)])
+        .send()
+        .await?;
+    Ok(())
+}
+
+fn from_storage() -> Result<Pin, Box<dyn std::error::Error>> {
+    let conn = Connection::open(dotenv!("DB_FILE"))?;
+
+    let mut stmt = conn
+        .prepare("SELECT title, author, link, description, tags  FROM 'pins' WHERE sent IS NULL ORDER BY id LIMIT 1")?;
+    let mut pins = stmt.query_map([], |row| {
+        let tags_string: String = row.get(4).unwrap();
+        Ok(Pin {
+            d: row.get(0)?,
+            a: row.get(1)?,
+            u: row.get(2)?,
+            n: row.get(3)?,
+            t: tags_string.split(',').map(|s| s.to_string()).collect(),
+        })
+    })?;
+
+    Ok(pins.nth(0).unwrap()?)
+}
+
 fn to_storage(pins: &Vec<Pin>) -> Result<(), Box<dyn std::error::Error>> {
     let mut conn = Connection::open(dotenv!("DB_FILE"))?;
 
@@ -68,6 +102,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     if op == "pull" {
         let pins = fetch_pins().await?;
         to_storage(&pins)?;
+    } else if op == "push" {
+        let p = from_storage()?;
+        post_to_tg_channel(p).await?;
     } else {
         panic!("only support pull for now");
     }
